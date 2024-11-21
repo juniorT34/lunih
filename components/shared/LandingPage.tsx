77283b939124411a35bd4a2bd025a436e09f9ef9
juniorT34 <jj1982268@gmail.com>
2024-11-21 13:26:15 +0200

    homepage, about section, landing section and footer

---
+++ components/shared/LandingPage.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import Image from 'next/image'
+
+export default function LandingPage() {
+  return (
+    <div className="relative h-screen">
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-white">
+        <h1 className="text-5xl font-bold">WELCOME TO LUNIH</h1>
+        <p className="mt-4 text-xl">A Hub for Innovation, Inspiration, and Endless Possibilities.</p>
+      </div>
+    </div>
+  )
+}
+
