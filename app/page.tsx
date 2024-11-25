3cd6b56383ee674a4373d52b0fb5b34e3fcf9545
juniorT34 <jj1982268@gmail.com>
2024-11-25 16:27:29 +0200

    card component for about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CardDesc from "@/components/shared/CardDesc";
 import Header from "@/components/shared/Header";
 import Link from "next/link";
 
@@ -11,7 +12,7 @@ export default function Home() {
       >
         {/* Overlay for the text */}
         <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center text-white">
-          <h1 className="text-6xl font-bold mb-4">Welcome to LUN<span className="text-primary-100">IH</span></h1>
+          <h1 className="text-6xl font-bold mb-4 text-center">Welcome to LUN<span className="text-primary-100">IH</span></h1>
           <p className="text-2xl mb-6 max-w-3xl text-center">
             We are thrilled to have you here.<br /> 
             Explore our hub for <br />
@@ -28,6 +29,28 @@ export default function Home() {
               Go to Hub
               </Link>
             </button>
+          </div>
+        </div>
+      </section>
+
+      {/* About Us Section */}
+      <section id="about" className="py-16 px-10 bg-gray-100 w-full h-screen">
+        <div className="container mx-auto text-center">
+          <h2 className="text-4xl font-bold mb-8">About Us</h2>
+          <p className="text-lg mb-12 max-w-4xl mx-auto">
+          Liepaja University New Idea Hub or LUNIH, is a platform where are showcased innovative ideas such as :
+          </p>
+          
+          {/* Cards Section */}
+          <div className="flex-col flex md:flex-row gap-8 items-center justify-center">
+            {/* Card 1 */}
+            <CardDesc title="Project Ideas" description="Individual innovative idea that can be used by anyone specially RTU liepaja university student"/>
+
+            {/* Card 2 */}
+            <CardDesc title="Bachelor/master thesis" description="Proposed thesis ideas that could be used in real world scenarios, could be build by student "/>
+
+            {/* Card 3 */}
+            <CardDesc title="Internships" description="Private entities and companies get to publish internship opportunities, so that student can easily apply"/>
           </div>
         </div>
       </section>
---
+++ components/shared/CardDesc.tsx
@@ -0,0 +1,27 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import React from "react";
+type CardDescProps = {
+    title: string,
+    description:  string
+}
+
+const CardDesc = ({title,description} : CardDescProps) => {
+  return (
+    <div className="bg-white w-80 shadow-lg rounded-lg p-6 hover:shadow-xl transition">
+      <h3 className="text-2xl font-semibold mb-4">{title}</h3>
+      <p className="text-gray-600">
+        {description}
+      </p>
+    </div>
+  );
+};
+
+export default CardDesc;
