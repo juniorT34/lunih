72b4e31a6bfb4375612797bed82ba610064ea98d
juniorT34 <jj1982268@gmail.com>
2025-01-06 17:35:07 +0200

    Refactor Post component to use a dedicated PageProps interface for improved type safety

--- app/hub/[id]/page.tsx
+++ app/hub/[id]/page.tsx
@@ -1,11 +1,16 @@
 import SinglePost from '@/components/shared/SinglePost'
 import React from 'react'
 
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
 
-const Post = ({params}:{params : {id: string}}) => {
+const Post = async ({ params }: PageProps) => {
   return (
     <div className=''>
-        <SinglePost postId={params.id}/>
+      <SinglePost postId={params.id}/>
     </div>
   )
 }
