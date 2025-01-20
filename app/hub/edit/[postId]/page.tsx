bc2420060c9e75ce7d563cd837830f384e2188bf
juniorT34 <jj1982268@gmail.com>
2025-01-20 04:17:31 +0200

    Refactor EditPost component to use Suspense for loading state

--- app/hub/edit/[postId]/page.tsx
+++ app/hub/edit/[postId]/page.tsx
@@ -1,19 +1,24 @@
 import EditPostElement from '@/components/shared/EditPostElement'
 import Footer from '@/components/shared/Footer'
 import Header from '@/components/shared/Header'
-import React from 'react'
+import { Suspense } from 'react'
 
+interface PageProps {
+  params: {
+    postId: string
+  }
+}
 
-const EditPost = ({ params }: { params: { postId: string } }) => {
-  
+function EditPost({ params}: PageProps) {
   return (
     <div className='bg-primary-50 bg-dotted-pattern bg-cover bg-center'>
-        <Header />
-        <EditPostElement postId={params.postId}/>
-        <Footer />
+      <Header />
+      <Suspense fallback={<div>Loading...</div>}>
+        <EditPostElement postId={params.postId} />
+      </Suspense>
+      <Footer />
     </div>
   )
 }
-
 
 export default EditPost
