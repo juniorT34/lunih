06d871dbef703b0987c33d373020d7db00ca5519
juniorT34 <jj1982268@gmail.com>
2025-01-20 02:58:34 +0200

    Update metadata for dashboard and main layout; enhance middleware to protect profile routes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,37 +1,31 @@
-import Header from '@/components/shared/Header'
-import Sidebar from '@/components/shared/Sidebar'
-// import Sidebar from '@components/shared/Sidebar'
-import React from 'react'
+import React from "react";
 
 const Dashboard = () => {
   return (
     <>
-    <div className="min-h-screen">
-      {/* <Header /> */}
-      <div className="flex">
-        {/* <Sidebar /> */}
-        <main className="flex-1 p-8">
-
-          <h1 className="text-2xl font-bold mb-6">Dashboard Overview</h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            <div className="p-6 bg-white rounded-lg shadow">
-              <h2 className="font-semibold mb-2">Total Posts</h2>
-              <p className="text-3xl font-bold">12</p>
+      <div className="min-h-screen">
+        <div className="flex">
+          <main className="flex-1 p-8">
+            <h1 className="text-2xl font-bold mb-6">Dashboard Overview</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              <div className="p-6 bg-white rounded-lg shadow">
+                <h2 className="font-semibold mb-2">Total Posts</h2>
+                <p className="text-3xl font-bold">12</p>
+              </div>
+              <div className="p-6 bg-white rounded-lg shadow">
+                <h2 className="font-semibold mb-2">joined projects</h2>
+                <p className="text-3xl font-bold">5</p>
+              </div>
+              <div className="p-6 bg-white rounded-lg shadow">
+                <h2 className="font-semibold mb-2">Pending requests</h2>
+                <p className="text-3xl font-bold">8</p>
+              </div>
             </div>
-            <div className="p-6 bg-white rounded-lg shadow">
-              <h2 className="font-semibold mb-2">joined projects</h2>
-              <p className="text-3xl font-bold">5</p>
-            </div>
-            <div className="p-6 bg-white rounded-lg shadow">
-              <h2 className="font-semibold mb-2">Pending requests</h2>
-              <p className="text-3xl font-bold">8</p>
-            </div>
-          </div>
-        </main>
+          </main>
+        </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
